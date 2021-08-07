bce19e28c6d508072f641dee890632903745fac2
misitebao <i@misitebao.com>
2021-08-07 12:00:03 +0800

    Publish npm package v0.0.8

---
+++ npm/index.js
@@ -0,0 +1,17 @@
+// Prompt after successful installation
+import chalk from 'chalk'
+
+const REPO = "https://github.com/tigateam/tiga-cli"
+const ISSUE = "https://github.com/tigateam/tiga-cli/issues"
+const WEBSITE = "https://cli.tigateam.org"
+
+console.log(`\nRepo: ${chalk.green.underline(REPO)}\n`)
+
+console.log(`Issue: ${chalk.green.underline(ISSUE)}\n`)
+
+console.log(`Website: ${chalk.green.underline(WEBSITE)}\n`)
+
+console.log(`${chalk.cyan(`You have successfully installed the Tiga CLI tool from npm, please start your show🚀.`)}\n`)
+
+console.log(`${chalk.cyan(`If you encounter problems in use, please read the official documentation.`)}\n`)
+
